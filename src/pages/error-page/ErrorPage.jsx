374fbaa7153127f0246b2bae6bb14a2d0272e239
Michel <baratella2@gmail.com>
2024-09-07 15:38:47 -0300

    updates to css

--- src/pages/error-page/ErrorPage.jsx
+++ src/pages/error-page/ErrorPage.jsx
@@ -3,15 +3,13 @@ import "./ErrorPage.css";
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.error(error);
 
   return (
     <div id="error-page" className="error-page">
-      <h1>Oops!</h1>
-      <p>Desculpe, ocorreu um erro inesperado!</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <div className="container">
+        <h1 className="heading">Oops!</h1>
+        <p className="description">Desculpe, ocorreu um erro inesperado.</p>
+      </div>
     </div>
   );
 };
